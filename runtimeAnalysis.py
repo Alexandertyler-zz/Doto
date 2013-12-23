47b5b4a26d14785d4ca5a4a7959cc2ad3c51ab38
Alexandertyler <Alexanderhtyler@gmail.com>
2013-12-22 16:55:07 -0800

    Initialized a dictionary for hero location tracking.

--- runtimeAnalysis.py
+++ runtimeAnalysis.py
@@ -1,13 +1,30 @@
 #! /usr/bin/python
 import tarrasque
+from pprint import pprint
 
 class Replay:
 	def __init__(self):
 		print("Initializing replay class.")
+		#initialize players and their heros as objects
+		self.playerPositions = dict()	
 
 	def loadReplay(fileName):
 		self.replay = tarrasque.StreamBinding.from_file(fileName)
 		print("Replay fully parsed.")
+
+	def initializeHeroDict():
+		for player in self.replay.players:
+			hero = player.hero
+			heroName = hero.name
+			initPosition = hero.position
+			self.playerPositions(heroName=initPosition)
+		pprint(self.playerPositions)
+
+def test():
+	replayClass = tarrasque.StreamBinding.from_file("test.dem")
+	replayClass.loadReplay("test.dem")
+	replayClass.initializeHeroDict()
+
 
 def parse(command):
 	if command == "q":
@@ -17,16 +34,22 @@ def parse(command):
 		print("q - quit\n intialize - Initialize a replay class\n loadReplay - Load a replay for analysis")
 
 	elif command == "initialize":
-		replay = Replay()
+		replayClass = Replay()
 
 	elif command == "loadReplay":
-		replay.loadReplay("test.dem")	
+		replayClass.loadReplay("test.dem")
+	
+	elif command == "initializeHeroDict":
+		replayClass.initializeHeroDict()
+
+	elif command == "test":
+		test()	
 
 	else:
 		print("Command was not recognized. Enter -h for a list of commands.")
-
+	
 
 if __name__=='__main__':
 	while True:
 		command = raw_input("Please enter a command: ")
-		parse(command)		
+		parse(command)
