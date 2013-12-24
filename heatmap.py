705c3b296a560e8c1b701c58075a073c0d163b5b
Alexandertyler <Alexanderhtyler@gmail.com>
2013-12-23 17:27:09 -0800

    Newer heatmap.

--- heatmap.py
+++ heatmap.py
@@ -1,20 +1,40 @@
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.mlab as mlab
 import numpy as np
+import numpy.random
 
-column_labels = list('0123')
-row_labels = list('ABCD')
-data = np.random.rand(4,4)
-fig, ax = plt.subplots()
-heatmap = ax.pcolor(data, cmap=plt.cm.Blues)
+n = 1e5
+x = y = x = y = np.linspace(-5, 5, 100)
+X, Y = np.meshgrid(x, y)
+Z1 = mlab.bivariate_normal(X, Y, 2, 2, 0, 0)
+Z2 = mlab.bivariate_normal(X, Y, 4, 1, 1, 1)
+ZD = Z2 - Z1
+x = X.ravel()
+y = Y.ravel()
+z = ZD.ravel()
+gridsize=30
+plt.subplot(111)
 
-# put the major ticks at the middle of each cell
-ax.set_xticks(np.arange(data.shape[0])+0.5, minor=False)
-ax.set_yticks(np.arange(data.shape[1])+0.5, minor=False)
+# if 'bins=None', then color of each hexagon corresponds directly to its count
+# 'C' is optional--it maps values to x-y coordinates; if 'C' is None (default) then 
+# the result is a pure 2D histogram 
 
-# want a more natural, table-like display
-ax.invert_yaxis()
-ax.xaxis.tick_top()
+plt.hexbin(x, y, C=z, gridsize=gridsize, cmap=cm.jet, bins=None)
+plt.axis([x.min(), x.max(), y.min(), y.max()])
 
-ax.set_xticklabels(row_labels, minor=False)
-ax.set_yticklabels(column_labels, minor=False)
+cb = plt.colorbar()
+cb.set_label('mean value')
+plt.show()   
+
+"""
+x = np.random.randn(8873)
+y = np.random.randn(8873)
+
+heatmap, xedges, yedges = np.hexbin(x, y, bins=50)
+extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+
+plt.clf()
+plt.imshow(heatmap, extent=extent)
 plt.show()
+"""
