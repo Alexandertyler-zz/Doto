3ef7c0a390087eeb34000877065fa1d5fabbc5f3
Alexandertyler <Alexanderhtyler@gmail.com>
2013-12-27 08:13:15 -0800

    Player Movement Graph initial commit.

--- heatmap.py
+++ heatmap.py
@@ -1,18 +1,25 @@
 import pylab as pl
+import matplotlib.pyplot as plt
 import numpy as np
 
-n = 300                                     #number of sample data
-x,y = np.random.rand(2,n)                   #generate random sample locations
+im = plt.imread("dota_map.jpg") #get the image we are laying over
+imgplot = plt.imshow(im)
+imgplot.set_interpolation('bicubic')
+plt.colorbar()
 
-pl.subplot(121)                             #sub-plot area 1 out of 2
-pl.scatter(x,y,lw=0,c='k')                  #draw sample points
-pl.axis('image')                            #necessary for correct aspect ratio
 
-pl.subplot(122)                             #sub-plot area 2 out of 2
+#n = 300                                     #number of sample data
+#x,y = np.random.rand(2,n)                   #generate random sample locations
 
-pl.hexbin(x,y,C=None,gridsize=15,bins=None,mincnt=1)        #hexbinning
+#pl.subplot(121)                             #sub-plot area 1 out of 2
+#pl.scatter(x,y,lw=0,c='k')                  #draw sample points
+#pl.axis('image')                            #necessary for correct aspect ratio
 
-pl.scatter(x,y,lw=0.5,c='k',edgecolor='w')  #overlaying the sample points
-pl.axis('image')                            #necessary for correct aspect ratio
+#pl.subplot(122)                             #sub-plot area 2 out of 2
+
+#pl.hexbin(x,y,C=None,gridsize=15,bins=None,mincnt=1)        #hexbinning
+
+#pl.scatter(x,y,lw=0.5,c='k',edgecolor='w')  #overlaying the sample points
+#pl.axis('image')                            #necessary for correct aspect ratio
 
 pl.show()                                   #to show the plot
--- runtimeAnalysis.py
+++ runtimeAnalysis.py
@@ -1,9 +1,13 @@
 #! /usr/bin/python
 
 import tarrasque
-#from pprint import pprint
+import pylab as pl
+import numpy as np
+
+
 
 class Replay:
+	
 	def __init__(self):
 		print("Initializing replay class.")
 		#initialize players and their heros as objects
@@ -33,23 +37,65 @@ class Replay:
 		for entry in self.playerPositions:
 			print(entry)
 
+	def fullGameHeroLocations(self):
+		for tick in replay.iter_ticks(start=game_start, end=game_end):
+			break
+
+	def playerMovementGraph(hero_name):
+		for player in self.replay.players:
+			if player.hero = hero_name:
+				distance_data = []
+				tick_data = []
+				prevX, prevY = 0
+				go_to_tick(game_start)
+				currX, currY = player.hero.position
+				#might have to change step size to get a better estimation of movement
+				for tick in replay.iter_ticks(start=game_start, end=game_end):
+					if not player.hero.is_alive:
+						currX, currY = 0
+						break
+					prevX, prevY = currX, currY
+					currX, currY = player.hero.location
+					dist_traveled = manhattan(currX, currY, prevX, prevY)
+					distance_data.append(dist_traveled)
+					tick_data.append(tick)
+				#call plotting function
+				#plt.plot(tick_data, distance_data)
+				#plt.show()
+				#plt.savefig("./playerMovementGraph.png")
+
 	def itemsPurchased(self):
 		for player in self.replay.players:
 			for item in player.hero.inventory:
 				self.itemList.append((item, item.purchase_time))
 		for item in self.itemList:
 			print(item)
+
+	def netGold(self):
+		for player in self.replay.players:
+			
 				
 class Heatmap:
-	def __init__(self):
-		self.heatmap = []
+	def __init__(self, x, y):
+		self.x = x #An array of tuples of locations
+		self.y = y
+
+	def plot(self):
+		#pl.subplot(121)
+		pl.hexbin(self.x, self.y)
+		pl.axis('image')
+		pl.show()
 
 def test():
-	replayClass = Replay() 
-	replayClass.loadReplay("test.dem")
-	replayClass.initializeHeroDict()
-	replayClass.timeStepAndLocation(1)
-	replayClass.itemsPurchased()
+	#replayClass = Replay() 
+	#replayClass.loadReplay("test.dem")
+	#replayClass.initializeHeroDict()
+	#replayClass.timeStepAndLocation(1)
+	#replayClass.itemsPurchased()
+	x = [1, 1, 2, 2, 3, 4, 2, 2, 5]
+	y = [1, 3, 2, 2, 4, 2, 1, 2, 3]
+	heatmap = Heatmap(x, y)
+	heatmap.plot()
 
 def parse(command):
 	if command == "q":
